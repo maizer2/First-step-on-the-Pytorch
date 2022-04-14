5c2e725a9f6383dca327be153473da4e71991bbd
maizer2 <jhw3979@gmail.com>
2022-04-14 15:09:28 +0000

    add png

--- Chapter3_Linear_Regression/LR.py
+++ Chapter3_Linear_Regression/LR.py
@@ -36,5 +36,6 @@ for i in range(num_epoch):
     param_list = list(model.parameters())
     #print(param_list[0].item(), param_list[1].item())
 plt.plot(x, y)
+plt.savefig('LR_dot')
 plt.plot(param_list)
-plt.savefig('LR')
+plt.savefig('LR_line')
